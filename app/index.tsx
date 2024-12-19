8f6343778c82b34eea68e7f36fa86228c20e0d14
Kevin Bui <kbui@moneymappress.com>
2024-12-18 19:37:43 -0500

    Got the tabs working and styled

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { StatusBar } from 'expo-status-bar';
+import { Link } from 'expo-router';
 
 const Index = () => {
   return (
    <View className='flex-1 items-center justify-center bg-white'>
-   <Text className='text-3xl'>Aora</Text>
+   <Text className='text-3xl font-pblack'>Aora!</Text>
    <StatusBar style="auto" />
+   <Link href='/home' style={{color: 'blue'}}>Go to Home</Link>
    </View>
   )
 }
