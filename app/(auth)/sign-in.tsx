5591dc47600a8397b112ddc0e687094f18cd469a
Kevin Bui <kbui@moneymappress.com>
2024-12-18 21:26:39 -0500

    Login screen

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,11 +1,59 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, Image } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import {images} from '../../constants'
+import FormField from '../components/FormField'
+import CustomButton from '../components/CustomButton'
+import { Link } from 'expo-router'
 
 const SignIn = () => {
+
+  const [form, setForm] = React.useState({
+    email: '',
+    password: ''
+  })
+
+  const [isSubmitting, setIsSubmitting] = React.useState<boolean>(false)
+
+  const submit = () => {
+  }
+
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className='bg-primary h-full'>
+      <ScrollView>
+        <View className='w-full justify-center min-h-[83vh] px-4 my-6'>
+          <Image source={images.logo} className='w-[115px] h-[35px] ' resizeMode='contain' />
+          <Text className='text-2xl text-white text-semibold mt-10 font-psemibold'>Log in to Aora</Text>
+          <FormField
+            title="Email"
+            value={form.email}
+            placeholder='Enter your email'
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+          <FormField 
+            title='Password'
+            value={form.password}
+            placeholder='Enter your password'
+            handleChangeText={(e:string) => setForm({...form, password: e})}
+            otherStyles='mt-7'
+            keyboardType='password'
+          />
+
+          <CustomButton 
+            title='Sign in'
+            handlePress={submit}
+            containerStyles='mt-7'
+            isLoading={isSubmitting}
+          />
+          <View className='justify-center p-5 flex-row gap-2'>
+            <Text className='text-gray-100 text-lg font-pregular'>Don't have an account?</Text>
+            <Link href='/sign-up' className='text-lg font-psemibold text-secondary'>Sign Up</Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
