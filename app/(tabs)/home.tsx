c7030dc4cf7c0b8420779eed5a1547555bc67bec
Kevin Bui <kbui@moneymappress.com>
2024-12-20 11:48:15 -0500

    some cleanup

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -4,7 +4,6 @@ import {
   FlatList,
   Image,
   RefreshControl,
-  Alert,
 } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
--- app/search/[query].tsx
+++ app/search/[query].tsx
@@ -2,15 +2,10 @@ import {
   View,
   Text,
   FlatList,
-  Image,
-  RefreshControl,
-  Alert,
 } from "react-native";
 import React, { useEffect } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { images } from "../../constants";
 import SearchInput from "../components/SearchInput";
-import Trending from "../components/Trending";
 import EmptyState from "../components/EmptyState";
 import useAppwrite from "@/lib/useAppwrite";
 import { searchPosts } from "@/lib/appwrite";
