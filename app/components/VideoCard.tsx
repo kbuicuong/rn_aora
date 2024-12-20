7d293a3c3876e8d3ae426ad543a37c4efe027858
Kevin Bui <kbui@moneymappress.com>
2024-12-20 10:24:19 -0500

    Have the video playing in videocard

--- app/components/VideoCard.tsx
+++ app/components/VideoCard.tsx
@@ -1,10 +1,21 @@
 import { View, Text, Image, TouchableOpacity } from "react-native";
 import React from "react";
 import { icons } from "@/constants";
+import { useVideoPlayer, VideoView } from "expo-video";
+import { useEvent } from "expo";
+
+const videoSource =
+  "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
 
 const VideoCard = (video: any) => {
+  const player = useVideoPlayer(videoSource, (player) => {
+    player.loop = true;
+    // player.play();
+  });
 
-  const [play, setPlay] = React.useState<boolean>(false);
+  const { isPlaying } = useEvent(player, "playingChange", {
+    isPlaying: player.playing,
+  });
 
   return (
     <View className="flex flex-col items-center px-4 mb-14">
@@ -19,30 +30,44 @@ const VideoCard = (video: any) => {
           </View>
 
           <View className="justifu-center flex-1 ml-3 gap-y-1">
-            <Text className="text-white font-psemibold text-sm" numberOfLines={1}>
+            <Text
+              className="text-white font-psemibold text-sm"
+              numberOfLines={1}
+            >
               {video.video.title}
             </Text>
-            <Text className="text-xs text-gray-100 font-pregular" numberOfLines={1}>
+            <Text
+              className="text-xs text-gray-100 font-pregular"
+              numberOfLines={1}
+            >
               {video.video.users.username}
             </Text>
           </View>
         </View>
 
         <View className="pt-2">
-          <Image
-            source={icons.menu}
-            className="w-5 h-5"
-            resizeMode="contain"
-          />
+          <Image source={icons.menu} className="w-5 h-5" resizeMode="contain" />
         </View>
       </View>
 
-      {play ? (
-        <Text className="text-white">Playing</Text>
-      ): (
-        <TouchableOpacity className="w-full h-60 rounded-xl mt-3 relative justify-center items-center"
-        activeOpacity={0.7}
-        onPress={() => setPlay(true)}
+      {isPlaying ? (
+        <VideoView
+          player={player}
+          allowsFullscreen
+          allowsPictureInPicture
+          style={{ width: '100%', height: 275 }}
+        />
+      ) : (
+        <TouchableOpacity
+          className="w-full h-60 rounded-xl mt-3 relative justify-center items-center"
+          activeOpacity={0.7}
+          onPress={() => {
+            if (isPlaying) {
+              player.pause();
+            } else {
+              player.play();
+            }
+          }}
         >
           <Image
             source={{ uri: video.video.thumbnail }}
