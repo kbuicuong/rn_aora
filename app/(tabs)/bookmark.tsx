6ac53c9684c11b31c03de8d66513008fa094cc85
Kevin Bui <kbui@moneymappress.com>
2024-12-21 16:52:55 -0500

    bookmark page started

--- app/(tabs)/bookmark.tsx
+++ app/(tabs)/bookmark.tsx
@@ -1,12 +1,56 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList, Image, RefreshControl } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
+import SearchInput from "../components/SearchInput";
+import Trending from "../components/Trending";
+import EmptyState from "../components/EmptyState";
+import useAppwrite from "@/lib/useAppwrite";
+import { getAllPosts, getLatestPosts } from "@/lib/appwrite";
+import VideoCard from "../components/VideoCard";
+import { useGlobalContext } from "@/context/GlobalProvider";
 
 const Bookmark = () => {
-  return (
-    <View>
-      <Text>Bookmark</Text>
-    </View>
-  )
-}
+  const { user } = useGlobalContext();
 
-export default Bookmark
+  const { data: posts, refetch } = useAppwrite(getAllPosts);
+  const { data: latestPosts } = useAppwrite(getLatestPosts);
+
+  const [refreshing, setRefreshing] = React.useState<boolean>(false);
+
+  const onRefresh = async () => {
+    setRefreshing(true);
+    await refetch();
+    setRefreshing(false);
+  };
+
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => <VideoCard video={item} />}
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4 space-y-6">
+            <Text className="text-2xl text-white font-psemibold mb-4">
+              Saved Videos
+            </Text>
+
+            <SearchInput />
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No Videos Found"
+            subtitle="Be the first one to upload a video"
+          />
+        )}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      />
+    </SafeAreaView>
+  );
+};
+
+export default Bookmark;
